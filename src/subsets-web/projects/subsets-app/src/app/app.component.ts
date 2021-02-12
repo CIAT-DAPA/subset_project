dbfd5233010fbad0142e0df266f78f950b1fbe3a
stevensotelo <stevenbetancurt@hotmail.com>
2021-02-12 15:14:25 -0500

    scalfolding subsets-web

---
+++ src/subsets-web/projects/subsets-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alliance-cgiar-org-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'subsets-app';
+}
