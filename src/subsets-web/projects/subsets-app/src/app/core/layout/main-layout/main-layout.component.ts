83fddec7b4ac57f80e1d7533eff663cf1ca3a268
stevensotelo <stevenbetancurt@hotmail.com>
2021-02-17 15:58:03 -0500

    working in the scalfolding of subsets web

---
+++ src/subsets-web/projects/subsets-app/src/app/core/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'alliance-cgiar-org-main-layout',
+  templateUrl: './main-layout.component.html',
+  styleUrls: ['./main-layout.component.scss']
+})
+export class MainLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
