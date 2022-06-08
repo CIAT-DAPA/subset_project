639b240c11004cb922d9709f264baf8a099f07f4
Khadija Aouzal <khadija.aouzal@gmail.com>
2022-06-08 13:51:30 +0100

    delete unused files and directories

--- src/subsets_api/indicators/scripts.py
+++
@@ -1,8 +0,0 @@
-from .models import *
-
-
-def getAccessionsById(id_list):
-    response_list = []
-    if id_list:
-        accessions = Accession.objects.filter(id__in=id_list)
-        return accessions
